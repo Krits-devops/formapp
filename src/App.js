d63705d1c1bf00f6aef34fa31cb1b478aad25286
Krits-devops <kritagyarm9@gmail.com>
2024-12-20 23:14:45 -0500

    Initialization

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+ function changeHandler(event){
+  console.log(event.target.value);
+ }
+
+  return (
+    <div className="App">
+      <form>
+        <input
+          type='text'
+          placeholder='first name'
+          onChange={changeHandler}
+          />
+          
+      </form>
+    </div>
+  );
+}
+
+export default App;
